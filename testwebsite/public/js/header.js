9659e44472ab49f7e9dd11cfed094196799b64b4
TripleCF <96793455+TripleCF@users.noreply.github.com>
2025-02-19 19:02:11 +0100

    Delete testwebsite directory

--- testwebsite/public/js/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navMobile = document.querySelector('.nav-mobile');
-    
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        navMobile.classList.toggle('active');
-    });
-}); 
